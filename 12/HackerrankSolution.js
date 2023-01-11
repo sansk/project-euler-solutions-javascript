a13ffaf8699998048f578eef86057f960bc25dc4
sansk <iamsangyk@gmail.com>
2023-01-11 11:10:18 +0530

    Update

--- 12/HackerrankSolution.js
+++ 12/HackerrankSolution.js
@@ -1,45 +1,41 @@
 function processData(input) {
     //Enter your code here
-    
-    const divisorCount = n => {
+    const divisors = n => {
         let count = 0;
-        for (let i = 1; i <= Math.floor(Math.sqrt(n)); i++) {
+        // Same as the properDivisor function. But instead of pushng to array,
+        // we straight away count the number of divisors.
+        for (let i = 1; i <= Math.sqrt(n); i++) {
             if (n % i === 0) {
-                if (i ** 2 !== n) {
-                    count += 2;
+                count += 2;
+                if (i === Math.sqrt(n)) {
+                    count--;
                 }
             }
-        }
-        if (Number.isInteger(Math.sqrt(n))) {
-            count = count + 1
         }
         return count;
     }
 
-    const findTriangularNum = n => {
-        return ((n * (n + 1)) / 2);
-    }
-
-    const findDivTriNum = () => {
-        let divNum = 0,
-            arr = [],
-            arrSize = 1000,
-            triNum = 0;
-
-        for(let i = 0; i <= arrSize; i++) {
-            while (divNum <= i) {
-                triNum++;
-                divNum = divisorCount(findTriangularNum(triNum));
+    // We make use of the upper bound specified in Hackerrank to pass all the test cases. 
+    // If N is a bigger number, the other solution would time-out.
+    // What is the value of the first triangle number to have over N divisors & 1 <= N <= 1000
+    const triNum = () => {
+        let arr = [];
+        let arrSize = 1000; // Upper bound specified in Hacker rank.
+        let divisorsNumber = 0;
+        let triangleNumber = 0;
+        for (let i = 0; i <= arrSize; i++) {
+            while (divisorsNumber <= i) {
+                triangleNumber++;
+                divisorsNumber = divisors((triangleNumber * (triangleNumber + 1) / 2));
             }
-            arr[i] = (findTriangularNum(triNum));
+            arr[i] = (triangleNumber * (triangleNumber + 1) / 2);
+            
         }
-        
+
         let values = input.split("\n");
-        
-        for (let i = 0; i < values.length; i++) {
+        for (let i = 1; i < values.length; i++) {
             console.log(arr[values[i]]);
         }
     }
-
-    findDivTriNum();
-}
+    triNum();
+} 
